4849c9cc00b11de96a73f6b902ed417d7469e054
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-12 17:02:54 -0300

    Update 11.c

--- listas/lista1/11.c
+++ listas/lista1/11.c
@@ -1,5 +1,5 @@
 /*
-. Escreva um programa que lê um número de dias e informa a quantidade correspondente em:
+Escreva um programa que lê um número de dias e informa a quantidade correspondente em:
 anos + semanas + dias.
 a. Considere:
 i. Ano = 365 dias
@@ -10,7 +10,15 @@ Dias: 427 = 1 ano(s), 8 semana(s) e 6 dia(s)
 #include <stdio.h>
 
 int main(){
-    
+    int dias, anos, semanas, resto;
+    printf("Insert a value for days\n> ");
+    scanf(" %d", &dias);
+
+    anos = dias/365;
+    semanas = (dias-365*anos)/7;
+    resto = dias - 365*anos - 7*semanas;
+
+    printf(">> %d years, %d weeks and %d days\n", anos, semanas, resto);
 
     return 0;
 }
