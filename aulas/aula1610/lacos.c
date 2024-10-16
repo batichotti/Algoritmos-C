e81c12b11e39085b05fc3c8705371d80f39ba776
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-16 18:51:47 -0300

    Update lacos.c

--- aulas/aula1610/lacos.c
+++ aulas/aula1610/lacos.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(){
+    int j;
+
+    for (int i = 0; i < 10; i++){
+        printf("i: %d\n", i);
+    }
+    
+    printf("\n");
+
+    while (j != 0){
+        printf("j:\n> ");
+        scanf(" %d", &j);
+    }
 
     return 0;
 }
