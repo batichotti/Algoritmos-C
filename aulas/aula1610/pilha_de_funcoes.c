e302d5323dfd514b3cd650c5ae17a16593e49430
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-16 19:58:47 -0300

    Create pilha_de_funcoes.c

---
+++ aulas/aula1610/pilha_de_funcoes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//mantem o registro de chamadas da funcao enquanto elas nao foram resolvidas
+
+int max(int a, int b){
+    if (a > b){
+        return a;
+    } else {
+        return b;
+    }
+    return 0;
+}
+
+int maiorDeTres(){
+    int a, b, c;
+    printf("Informe 3 valores:\n> ");
+    scanf(" %d %d %d", &a, &b, &c);
+
+    printf("Max: %d\n", max(max(a,b),c));
+    return 0;
+}
+
+int main(){
+    maiorDeTres();
+    return 0;
+}
