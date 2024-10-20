0574f759a3a32daac8cd572a2c9e27ba8d4672e7
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-20 18:06:17 -0300

    Update 2.c

--- listas/lista4/2.c
+++ listas/lista4/2.c
@@ -6,9 +6,11 @@ Escreva um programa que imprime a tabela ASC com valores em decimal (%03d), hexa
 */
 
 int main(){
-    printf("%03s %03s %03s %03s\n", "DEC", "OCT", "HEX", "CHR");
-    for (int i = 33; i <= 126; i++){
-        printf("%3d %3o %3X %2c\n", i, i, i, i);
+    printf("%03s %03s %03s %03s\n", "DEC", "HEX", "CHR");
+    for (int i = 33; i <= 64; i++){
+        printf("%3d %3X %2c ", i, i, i);
+        printf("%3d %3X %2c ", i+31, i+31, i+31);
+        printf("%3d %3X %2c\n", i+62, i+62, i+62);
     }
     return 0;
 }
