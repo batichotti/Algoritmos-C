e5995c5de52a1dffbd02c88b3f54275c45f9d537
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-21 17:14:17 -0300

    Create vetores_parametros.c

---
+++ aulas/aula10_21/vetores_parametros.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_vector(int length, int* vector){
+
+    for (int i = 0; i < length; i++){
+        printf("%d\n", vector[i]);
+    }
+    printf("\n");
+}
+
+void set_vector(int n, int* vector){
+    //insere valores no vetor
+    for (int i = 0; i < n; i++){
+        vector[i] = rand() % 10;
+    }
+
+    printf("\n");
+}
+
+int main(){
+    int n;
+
+    printf("> ");
+    scanf(" %d", &n);
+
+    int vector[n];
+
+    set_vector(n, vector);
+
+    print_vector(n, vector);
+    return 0;
+}
