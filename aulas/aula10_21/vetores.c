90aadc875e8486df10170e964fa0ab8a112cc1a9
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-21 16:19:45 -0300

    Create vetores.c

---
+++ aulas/aula10_21/vetores.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int v[16], n, vec_variable[n];
+    n = 64;
+
+    for (int i = 0; i < n; i++){
+        printf("%d\n", vec_variable[i]);
+    }
+    
+    return 0;
+}
