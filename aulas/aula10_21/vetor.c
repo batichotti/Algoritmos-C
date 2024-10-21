a31bc88ee3b278647ef50140f8ed052d00f6b1ee
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-21 16:37:00 -0300

    Update vetor.c

--- aulas/aula10_21/vetor.c
+++ aulas/aula10_21/vetor.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
 
 int main(){
-    int v1[10]; // 0-9
+    int v1[10] = {0}; // 0-9
 
     int n2;
-    printf("> ");
+    printf("tamanho de v2> ");
     scanf(" %d", &n2);
     int v2[n2]; // variavel de tamanho variavel
 
     int v3[] = {1,552,0,-1,7,99};
 
+    for (size_t i = 0; i < 10; i++){
+        v1[i] = 9;
+    }
+
+    //mostrando os lixos de memoria
+    for (int i = 0; i < 10; i++){
+        printf("%d\n", v1[i]);
+    }
+    
+    printf("\n");
+
     for (int i = 0; i < n2; i++){
         printf("%d\n", v2[i]);
     }
+
+    printf("\n");
+
+    for (int i = 0; i < 6; i++){
+        printf("%d\n", v3[i]);
+    }
     
+    printf("Fim do programa :)");
     return 0;
 }
