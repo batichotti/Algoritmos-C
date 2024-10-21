047dc68e247418507e10e0cba6f07a225f25b662
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-21 16:27:53 -0300

    Update vetores.c

--- aulas/aula10_21/vetores.c
+++ aulas/aula10_21/vetores.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    int v[16], n, vec_variable[n];
+    int v[16], n;
+    v[13] = 13;
     n = 64;
+    int vec_variable[n];
+    int v0[13] = {0}; // vetores de valor fixo se preenchem sozinhos com 0 nesse caso (preenche a primeira com o valor dito e o resto mandatoriamente com zeros)
+    int v_misterioso = {1,2,34,5,6,77,8,99,-1};
 
     for (int i = 0; i < n; i++){
         printf("%d\n", vec_variable[i]);
