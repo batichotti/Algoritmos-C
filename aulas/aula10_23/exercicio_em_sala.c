0b93b5c0c50c7983305aef92595fcbbd46aef227
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-23 19:08:53 -0300

    Update exercicio_em_sala.c

--- aulas/aula10_23/exercicio_em_sala.c
+++ aulas/aula10_23/exercicio_em_sala.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+void imprime_vetor(int n, int* vector){
+    for (int i = 0; i < n; i++){
+        printf("%d\n", vector[i]);
+    }
+}
+
 int main(){
     int n = 20;
-    int v[n];
+    int v[n]; //load with memory trash
 
-    for (int i = 0; i < n; i++){
-        printf("%d\n", v[i]);
-    }
+    imprime_vetor(n, v);
     return 0;
 }
