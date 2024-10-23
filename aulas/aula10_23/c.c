d8f11d6c95f78065a3a399259baf81c9b723ca88
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-23 19:01:18 -0300

    Update c.c

--- aulas/aula10_23/c.c
+++ aulas/aula10_23/c.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void print_even(int n, int * vector){
+    for (int i = 0; i < n; i++){
+        if (vector[i] % 2 == 0){
+                printf("%d \n", vector[i]);
+        }
+        
+    }
+}
 
 int main(){
-    
+    int n;
 
+    printf("Input a value for the length of the array:\n> ");
+    scanf(" %d", &n);
+    int v[n];
 
+    for (int i = 0; i < n; i++){
+        v[i] = rand() % 10;
+    }
+
+    print_even(n, v);
     return 0;
 }
