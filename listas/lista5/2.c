616f8971dc68acfa70da66aa335a293443f8f30f
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-23 19:28:29 -0300

    Create 2.c

---
+++ listas/lista5/2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void set_positive(int n, int* vector){
+    for (int i = 0; i < n; i++){
+        if (vector[i] < 0){
+            vector[i] = (-1)*vector[i];
+        }
+    }
+}
+
+int main(){
+    int n;
+    printf("Len of your array:\n> ");
+    scanf(" %d", &n);
+
+    int v[n];
+    for (int i = 0; i < n; i++){
+        v[i] = rand() % 100 * (-1);
+    }
+
+    set_positive(n, v);
+    for (int i = 0; i < n; i++){
+        printf("%d ", v[i]);
+    }
+    
+    return 0;
+}
