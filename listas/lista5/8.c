a0bb7532a15e85256b99901a7ec84cb4838a0d6c
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-30 18:40:20 -0300

    Update 8.c

--- listas/lista5/8.c
+++ listas/lista5/8.c
@@ -4,11 +4,10 @@ Escreva uma função que recebe um vetor e inverte os seus elementos. OBS: o có
 apenas imprimir o vetor ao contrário, mas sim, inverter os elementos no próprio vetor.
 */
 
-int* inverter_vetor(int n, int* vector){
+int* inverter_vetor(int n, int* vector, int* reversed){
     for (int i = n-1; i <= 0; i--){
         reversed[i-n+1] = vector[i];
     }
-    
 
 }
 
@@ -18,12 +17,13 @@ int main(){
     int reversed[n];
 
     for (int i = 0; i < n; i++){
-        v[i] = i;
+        v[i] = 1;
     }
     
-    inverter_vetor(n, v);
+    inverter_vetor(n, &v, &reversed);
 
     for (int i = 0; i < n; i++){
+        printf("%d\n", reversed[i]);
     }
     
 }
