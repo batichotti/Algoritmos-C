71628de9bde76b4965c04fcf2c7a64110e78eac9
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-30 19:38:52 -0300

    Update strings.c

--- aulas/aula10_30/strings.c
+++ aulas/aula10_30/strings.c
@@ -6,6 +6,13 @@ int length(char* array){
     return i;
 }
 
+void print_str(char* str){
+    for (int i = 0; str[i] != 0; i++){
+        printf("%c-", str[i]);
+    }
+    printf("\b \n");
+}
+
 int main(){
     char str[] = "Computaria"; //10 + \0
     char another_str[10]; //9 letras + \0
@@ -15,19 +22,14 @@ int main(){
     printf("%s\n", str);
     printf("%s\n", string_burra);
 
-    for (int i = 0; str[i] != 0; i++){
-        if (str[i+1] == 0){
-            printf("%c", str[i]); 
-        } else {
-            printf("%c-", str[i]);
-        }
-    }
-    
-    printf("\n");
-    for (int i = 0; str[i] != 0; i++){
-        printf("%c-", str[i]);
-    }
-    printf("%c \n", 8);
+    // for (int i = 0; str[i] != 0; i++){
+    //     if (str[i+1] == 0){
+    //         printf("%c", str[i]); 
+    //     } else {
+    //         printf("%c-", str[i]);
+    //     }
+    // }
+    // printf("\n");
 
     printf("%d", length(str));
 
