6a6ff6f188a1c06ca694a570fb6475ff86e83916
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-30 20:05:01 -0300

    Update string_lib.c

--- aulas/aula10_30/string_lib.c
+++ aulas/aula10_30/string_lib.c
@@ -5,12 +5,14 @@ int main(){
     char str[] = "Dai jiro su me chi metee";
 
     long length = strlen(str);
-    char copy[25];
+    char copy[150];
     strcpy(copy, str);
     int boolean = strcmp(str, copy); // se a da esquerda for menor retorna negativo, se a da direita for maior (vem antes) retorna positivo
 
     char new_str[length+1];
-    strcat(str, "eeeeeeeeeeeeeeeeeeeeeee");
+    strcat(new_str, "eeeeeeeeeeeeeeeeeeeeeee");
+
+    sprintf(copy, "Teto %.2f aaaa", 7.31415926535); //converte coisas
 
     return 0;
 }
