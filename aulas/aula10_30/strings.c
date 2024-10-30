eea745e4c9c21c37d0f9b97381f5429a3bb734d5
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-30 19:25:48 -0300

    Update strings.c

--- aulas/aula10_30/strings.c
+++ aulas/aula10_30/strings.c
@@ -3,11 +3,26 @@
 int main(){
     char str[] = "Computaria"; //10 + \0
     char another_str[10]; //9 letras + \0
+    another_str[2] = 'K';
     //veteres de caracteres não tem \0 no final, string tem o \0
     char string_burra[] = {'O', 'i', 0};
 
     printf("%s\n", str);
     printf("%s\n", string_burra);
+
+    for (int i = 0; str[i] != 0; i++){
+        if (str[i+1] == 0){
+            printf("%c", str[i]); 
+        } else {
+            printf("%c-", str[i]);
+        }
+    }
+    
+    printf("\n");
+    for (int i = 0; str[i] != 0; i++){
+        printf("%c-", str[i]);
+    }
+    printf("%c \n", 8);
 
     return 0;
 }
