fffcd96c89a87af38f36d27c1ba992c930603690
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-03 10:27:32 -0300

    Update 17.c

--- listas/lista5/17.c
+++ listas/lista5/17.c
@@ -13,19 +13,32 @@ void min_bills(int value, int n, int* bills){
         count[i] = 0;
     }
 
-    for (int i = n; soma != value; i++){
-        count[i] += value/bills[i];
+    for (int i = 0; i < n; i++){ //ordenando crescente bills
+        for (int k = 0; k < n-1; k++) {
+            if (bills[k] > bills[k+1]){
+                int escopo = bills[k];
+                bills[k] = bills[k+1];
+                bills[k+1] = escopo;
+            }
+        }
+        
+    }
+
+    for (int i = n-1; i >= 0; i--){
+        count[i] = (value) / (bills[i]);
+        value = value - count[i] * bills[i];
     }
     
+
     for (int i = 0; i < n; i++){
-        printf("%d x %d", count[i], bills[i]);
+        printf("%d x %d\n", count[i], bills[i]);
     }
-    
+
 }
 
 int main(){
-    int n = 7;
-    int v[] = {1, 5, 10, 20, 50, 100, 200}; //ordem crescente
+    int n = 8;
+    int v[] = {5, 1, 10, 200, 20, 100, 50, 2};
     
     min_bills(209, n, v);
 }
