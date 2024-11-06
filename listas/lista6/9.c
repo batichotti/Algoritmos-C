0bef6eb02e9192fc6baefd00738eeb4743035b32
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-06 20:09:16 -0300

    Update 9.c

--- listas/lista6/9.c
+++ listas/lista6/9.c
@@ -17,8 +17,8 @@ int in(char* v, char elem){
 
 void string_to_upper_case(char* str){
     for (int i = 0; str[i] != 0; i++){
-        if ( str[i] >= 97 && str[i] <= 122 ){
-            str[i] = str[i] - 'a' + 'A';
+        if ( str[i] >= 'a' && str[i] <= 'z' ){
+            str[i] -= 32;
         }
     }
 }
