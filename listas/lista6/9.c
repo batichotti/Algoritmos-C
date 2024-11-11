e28bf591117bb27606da86467320a9cceb3415cd
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-11 17:00:56 -0300

    Update 9.c

--- listas/lista6/9.c
+++ listas/lista6/9.c
@@ -6,10 +6,11 @@ tabela ASCII (26 possibilidades), bem como, seu percentual sobre o todo (descons
 espaços). A contagem deve desconsiderar letras maiúsculas e minúsculas.
 */
 
-int in(char* v, char elem){
-    for (int y = 0; v[y] != 0; y++){
-        if (v[y] == elem){
-            return y;
+int where_in_vetor(char* vetor, char elem){
+
+    for (int i = 0; vetor[i] != 0; i++){
+        if (vetor[i] == elem){
+            return i;
         }
     }
     return -1;
@@ -24,18 +25,30 @@ void string_to_upper_case(char* str){
 }
 
 void string_count(const char* str){
-    int counter[strlen(str)];
+    int counter[26] = {0};
+    char alfabeto[] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X', 'Y', 'Z', '\0'};
     
     char copy[strlen(str)];
     strcpy(copy, str);
+    string_to_upper_case(copy);
+    int total_letras = 0;
 
-    for (int i = 0; copy[i] != 0; i++){  
-
+    for (int i = 0; copy[i] != 0; i++){
+        int idx = where_in_vetor(alfabeto, copy[i]);
+        if ( idx != -1 ) {
+            counter[idx]++;
+            total_letras++;
+        }
     }
+
+    for (int i = 0; alfabeto[i] != 0; i++){
+        printf("%c - %d = %.2f%\n", alfabeto[i], counter[i], ((float) counter[i]/total_letras)*100);
+    }
+    
 }
 
 int main(){
-    string_count("Em dezembro de oitenta e um");
+    string_count("Em dezembro de oitenta e um, Botou os Ingleses na Roda");
 
     return 0;
 }
