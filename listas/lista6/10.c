2c77b38933568b346db0c0cde103a88e11a6d0c1
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-16 14:56:49 -0300

    Update 10.c

--- listas/lista6/10.c
+++ listas/lista6/10.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
+#include <string.h>
 /*
 Escreva uma função que remove os espaços que possam existir antes e depois de uma string.
 Neste caso, a função deverá, necessariamente, modificar a string de entrada str.
 */
 
-void string_trim(char* str){
+void string_trim(char* str) {
     int i;
-    for (i = 0; str[i+1] != 0; i++){
-        for (int j = 0; j != i; j++){
-            if ( str[i] == ' ' && str[i+1] != ' '){
-                str[i] = str[i+1];
-                str[i+1] = ' ';
-                break;
-            }
+    for (i = 0; str[i] != 0; i++){ // encontra o primeiro caracter diferente de espaço
+        if (str[i] != ' '){
+            break;
         }
     }
-    
-    for (int i = 0; str[i] != 0; i++){
-        /* code */
+
+    int j;
+    for (j = strlen(str)-1; j > 0; j--){ //encontra o último caracter diferente de espaço
+        if (str[j] != ' '){
+            break;
+        }
     }
-    
+
+    int k;
+    for (k = 0; k+i <= j; k++){ // reconstroi a string sem os espacos
+        str[k] = str[k+i];
+    }
+    str[k] = 0;
 }
 
-int main(){
-    char str[] = "   em      dez e m b r     o    de      oitenta e  um    ";
+int main() {
+    char str[] = "         em dezembro de oitenta e um        ";
     string_trim(str);
-    printf("%s", str);
+    printf("%s\n", str);
 
     return 0;
 }
