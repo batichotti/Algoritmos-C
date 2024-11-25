3a15052a2a42544e9827cd274adc21e2be750d0d
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-25 16:07:44 -0300

    create matriz.c

---
+++ aulas/aula11_25/matrizes.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+
+*/
+
+int main(){
+
+    // Vetor
+    int n = 10;
+    int v[n];
+
+    //Matriz
+     
+    int v[3][4] = { //3 linhas x 4 colunas + da pra omitir a primeira dimensão + v[linha][coluna]
+        {0,1,2,3},
+        {4,5,6,7},
+        {8,9,10,11}
+    };
+
+    return 0;
+}
