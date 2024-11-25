27f132dacde9d6c4da5ab81c5b378f978949a7a0
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-25 16:11:16 -0300

    Update matrizes.c

--- aulas/aula11_25/matrizes.c
+++ aulas/aula11_25/matrizes.c
@@ -11,12 +11,19 @@ int main(){
     int v[n];
 
     //Matriz
-     
     int v[3][4] = { //3 linhas x 4 colunas + da pra omitir a primeira dimensão + v[linha][coluna]
         {0,1,2,3},
         {4,5,6,7},
         {8,9,10,11}
     };
+
+    for (int i = 0; i < 3; i++){ //linhas
+        for (int j = 0; j < 4; j++){ //colunas
+            printf("%d ", v[i][j]);
+        }
+        printf("\n");
+    }
+    
 
     return 0;
 }
