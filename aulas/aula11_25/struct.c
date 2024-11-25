accfb4bde76372a86bcbc981b21b77cbb339f89c
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-25 17:02:39 -0300

    Update struct.c

--- aulas/aula11_25/struct.c
+++ aulas/aula11_25/struct.c
@@ -10,7 +10,9 @@ struct Person{
 typedef struct Person Person;
 
 int main(){
-    Person p1 = {2, "Jhon Doe", "john.doe@email.com"};
+    Person p1 = {2, "Jhon Doe", "john.doe@email.com", "a2222222"};
+
+    printf("{%d, %s, %s, %s}\n", p1.id, p1.nome, p1.email, p1.ra);
 
     return 0;
 }
