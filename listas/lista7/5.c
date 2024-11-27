36f165f22c682b4a68bb7bb8274dfd54a30602bb
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-27 14:58:45 -0300

    Update 5.c

--- listas/lista7/5.c
+++ listas/lista7/5.c
@@ -9,15 +9,17 @@ void int_values(int rows, int cols, int m[rows][cols], int start, int step){
     int iterador = 0;
     for (int i = 0; i < rows; i++){
         for (int j = 0; j < cols; j++){
-            m[i][j] = 10 + iterador*step;
+            m[i][j] = start + iterador*step;
             iterador++;
         }
     }
 }
 
+
 int main(){
     int matriz[3][3];
 
-    int_values(3,3,matriz,10, 2);
+    int_values(3,3,matriz, 10, 2);
+    printf("%d", matriz[2][2]);
     return 0;
 }
