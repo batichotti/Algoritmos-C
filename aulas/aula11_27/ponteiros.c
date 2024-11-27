e0c55d6f7520a6e85c0bfcd57925917fe80a84d3
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-27 19:26:20 -0300

    Update ponteiros.c

--- aulas/aula11_27/ponteiros.c
+++ aulas/aula11_27/ponteiros.c
@@ -3,9 +3,10 @@
 int main(){
     int a = 5;
     char b = 'F';
-    printf("&a: %p\n", &a);
-    printf("&b: %p\n", &b);
+    printf(" &a: %d -> %p\n", a, &a);
+    printf(" &b: %c -> %p\n", b, &b);
 
-    
+    int* p_a = &a; // o ponteiro avisa o tipo de dado contido no endereço (tamanho varia com a arquitetura)
+    printf("p_a: %d -> %p\n", *p_a, p_a);
     return 0;
 }
