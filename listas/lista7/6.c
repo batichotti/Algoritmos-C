3156d335b34a7f6f5c79e272e845b1733fb86132
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-27 15:06:11 -0300

    Create 6.c

---
+++ listas/lista7/6.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Escreva uma função que inicia uma matriz com valores aleatórios, sorteados entre min e max, isto
+é [min..max]. Utilize a função rand() da biblioteca <stdlib.h> para obter os valores:
+*/
+
+void matrix_random(int rows, int cols, int m[rows][cols], int min, int max){
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            m[i][j] = (rand() % (max-min))+min;
+        }
+    }
+}
+
+void print_matrix(int rows, int cols, int m[rows][cols]){
+
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            printf("%2d ", m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int matriz[3][3] = {};
+
+    matrix_random(3,3,matriz,5,50);
+    
+    print_matrix(3,3,matriz);
+    return 0;
+}
