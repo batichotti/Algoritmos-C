0ccb434bdc96104dfac20841c1ee6920aa3aa36d
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-11-28 21:23:11 -0300

    Update 8.c

---
+++ listas/lista7/8.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+
+*/
+
+void matrix_random(int rows, int cols, int m[rows][cols], int min, int max){
+    for (int i = 0; i < rows; i++){
+        for (int j = 0; j < cols; j++){
+            m[i][j] = (rand() % (max-min))+min;
+        }
+    }
+}
+
+void put_totals(int rows, int cols, int m[rows][cols]){
+    for (int i = 0; i < rows; i++){
+        int soma = 0;
+        int j;
+        for (j = 0; j < cols-1; j++){
+            soma += m[i][j];
+        }
+        m[i][j] = soma;
+    }
+}
+
+void print_matrix(int rows, int cols, int m[rows][cols]){
+    for (int i = 0; i < rows; i++){
+        int soma = 0;
+        int j;
+        for (j = 0; j < cols; j++){
+            printf("%d ", m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    int matriz[3][3] = {};
+
+    matrix_random(3,3,matriz,5,50);
+    
+    put_totals(3,3,matriz);
+
+    print_matrix(3,3,matriz);
+    return 0;
+}
