8b3f187989d79f510eef95120b40cff7005c37b3
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-12-06 19:41:48 -0300

    Update 20.c

--- listas/lista7/20.c
+++ listas/lista7/20.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 /*
 Escreva uma função que conta e devolve o número de ocorrências de uma palavra word em um
 array de n linhas de texto (strings de até len-1 letras).
 */
 
-int count_occurrences(int n, int len, char lines[n][len], char word[]){
-    int acumulador = 0;
+int count_occurrences(int n, int len, char lines[n][len], char word[]) {
+    int count = 0;
 
-    return acumulador;
+    for (int i = 0; i < n; i++) {
+        int line_length = strlen(lines[i]);
+        int word_length = strlen(word);
+
+        for (int j = 0; j <= line_length - word_length; j++) {
+            int k;
+            for (k = 0; k < word_length; k++) {
+                if (lines[i][j + k] != word[k]) {
+                    break;
+                }
+            }
+            if (k == word_length) {
+                count++;
+            }
+        }
+    }
+
+    return count;
 }
-
 int main() {
     char v[5][200] = {
     "computer programming is the process of designing and building an executable computer program for accomplishing a specific computing task.",
