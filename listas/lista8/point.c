3f286c7eeac9e33f71b631f06ad787911eba82f4
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-12-11 13:19:16 -0300

    Update point.c

--- listas/lista8/point.c
+++ listas/lista8/point.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 /*
 Escreva uma função que recebe um ponto x,y (Point) e o imprime com 2 casas de precisão.
+Escreva uma função que recebe dois pontos x,y (Point) e devolve a distância entre os mesmos
+Escreva uma função que recebe um vetor de pontos (Point), bem como seu tamanho, e informa
+(imprime) a distância a cada dois pontos consecutivos do vetor.
+Escreva uma função que recebe um vetor de pontos (Point) e os inicia com valores float aleatórios
+entre [-50, 50].
 */
 
 struct point {
@@ -11,14 +19,36 @@ struct point {
 typedef struct point Point;
 
 void print_point(Point p){
-    printf("(%.2f, %.2f)", p.x, p.y);
+    printf("(%.2f, %.2f)\n", p.x, p.y);
+}
+
+float distance(Point p1, Point p2){
+    return sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
+}
+
+void distance_vector(int n, Point v[]){
+    for (int i = 0; i <= n-1; i++){
+        printf("%f\n", distance(*(v+i), *(v+i+1)));
+        i++;
+    }
 }
 
 int main(){
+    Point p0 = {6.66, 3.14159265359278};
 
-    Point p = {6.66, 3.14159265359278};
+    print_point(p0);
 
-    print_point(p);
+    Point p1 = {-2.0f, 7.5f};
+    Point p2 = {5.0f, 12.4f};
+    float dist = distance(p1, p2);
 
+    printf("distancia: %f\n", dist);
+
+    Point p3 = {-1.0f, -1.0f};
+    Point array[] = {p0, p1, p2, p3};
+    distance_vector(4, array);
+
+    Point vp[10];
+    random_points(10, vp);
     return 0;
 }
