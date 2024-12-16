d3962f6df4a5fc5996c0e21e6f56fc4604041915
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-12-16 16:46:34 -0300

    Update malloc.c

--- aulas/aula12_16/malloc.c
+++ aulas/aula12_16/malloc.c
@@ -3,6 +3,7 @@
 // Malloc aloca memória na heap
 // Malloc é utilizado quando se precisa criar algo dentro de uma função e manter vivo até fora deste escopo de criação
 // MALLOC PRECISA ESTAR VINCULADO A UM PONTEIRO
+// Se a alocação der errado o retorno é NULL
 
 char* fn(int amount){
     //amount in MegaBytes(MB)
@@ -28,7 +29,15 @@ int main(int argc, char const *argv[]){
     p = fn(100);
 
     printf("%c", p[2]);
+
+    int* p = create(10);
+
+    int* newp = kalloc();
+
+    int* newnewp = realloc(p, 15*sizeof(int));
+
     free(p);
+    p = NULL; // Boa prática (anular ponteiro depois do free)
     // printf("%c", p[2]); // Retorna segmentation fault
 
     return 0;
