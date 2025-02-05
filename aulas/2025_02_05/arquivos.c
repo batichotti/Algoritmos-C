580a56039fe8df84fa5c56c40f49d362a1eca2ea
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-02-05 18:58:34 -0300

    Update arquivos.c

--- aulas/2025_02_05/arquivos.c
+++ aulas/2025_02_05/arquivos.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+
 int main(int argc, char const *argv[]) {
 
+    FILE* file;
+    char* path = "texto.txt";
+    file = fopen(path, "r");
+
+    char c = fgetc(file);
+    for (int i = 0; c != EOF; i++){
+        printf("%c", c);
+        c = fgetc(file);
+    }
+    
     return 0;
 }
