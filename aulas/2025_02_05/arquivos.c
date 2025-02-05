984ed91443da407a07ea909bfc1cf9c01cf3b00d
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-02-05 19:12:23 -0300

    Update arquivos.c

--- aulas/2025_02_05/arquivos.c
+++ aulas/2025_02_05/arquivos.c
@@ -5,13 +5,20 @@ int main(int argc, char const *argv[]) {
 
     FILE* file;
     char* path = "texto.txt";
-    file = fopen(path, "r");
+    file = fopen(path, "w");
 
-    char c = fgetc(file);
-    for (int i = 0; c != EOF; i++){
-        printf("%c", c);
-        c = fgetc(file);
+    if (file == NULL){
+        printf("ERRO: Acesso ao local de escrita do arquivo inválido.");
+        return 1;
     }
     
+    char* text_to_replace = "Fernanda Torres\nAinda Estou Aqui\n";
+
+    for (int i = 0; text_to_replace[i] != 0; i++){
+        fputc(text_to_replace[i], file);
+    }
+
+    fclose(file);
+
     return 0;
 }
