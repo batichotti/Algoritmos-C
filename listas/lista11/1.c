eb808916794f52ebe49cdc63c5571046ffde12ef
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-02-08 20:18:19 -0300

    Update 1.c

---
+++ listas/lista11/1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+// Escreva uma função que verifica se o arquivo de caminho informado existe (retorna 1). Caso
+// contrário, retorna 0.
+
+int file_exists(const char* filepath){
+    FILE* file;
+    file = fopen(filepath, "r");
+
+    if (file == NULL){
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+
+int main(int argc, char const *argv[]){
+
+    int i = file_exists("pokemon.txt");
+    if (i == 1){
+        printf("O arquivo existe");
+    } else {
+        printf("O arquivo não existe");
+    }
+
+    return 0;
+}
