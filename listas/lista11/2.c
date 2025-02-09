d2795b01f7e2d3a6b4cc08ff66d055f0bf0cce67
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-02-09 13:09:54 -0300

    .

---
+++ listas/lista11/2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+// Escreva uma função que receba um caminho de arquivo. Se o mesmo existir, faz nada. Se o
+// arquivo não existir, tenta criá-lo. A função deve retornar 1 (um) caso arquivo exista ou tenha sido
+// criado e, 0 (zero), caso contrário.
+
+int check_or_create(const char *filepath){
+    FILE* file = fopen(filepath, "a+");
+
+    if (file == NULL){
+        return 0;
+    } else {
+        return 1;
+    }
+    
+}
+
+int main(){
+    int i = check_or_create("kakaroto.txt");
+    printf("%d", i);
+
+    return 0;
+}
