1ad0c6b6a877a80c6173750e5691d928db4629ed
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2025-02-19 12:33:52 -0300

    Update movie.c

--- listas/lista13/movie.c
+++ listas/lista13/movie.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 // Para os exercícios que seguem, considere o tipo estruturado Movie, que deverá ser utilizado para
 // manipular arquivos de registros de filmes:
 typedef struct {
@@ -30,6 +31,53 @@ int count_movies(const char* filepath){
     return (int) (byte_size/sizeof(Movie));
 }
 
+// Escreva uma função que lê um registro de filme de um arquivo, na posição indicada (0, 1, 2, …). A
+// função deve retornar uma cópia do registro. Se a posição for inválida, a função deve retornar um
+// registro preenchido com zeros. OBS: você não deve trazer todos os registros para a RAM, em um
+// array. A função deve ler somente somente o registro em questão.
+Movie read_movie(const char* filepath, int pos){
+    FILE* file = fopen(filepath, "r");
+    if (file == NULL){
+        printf("Deu problema pra abrir o arquivo\n");
+        Movie erro;
+        erro.id = -1;
+        strcpy(erro.name, "erro");
+        return erro;
+    }
+    
+    fseek(file, sizeof(Movie)*pos, SEEK_SET);
+    Movie movie;
+    fread(&movie, sizeof(Movie), 1, file);
+    rewind(file);
+    fclose(file);
+    return movie;
+}
+
+// Escreva uma função que retorna a posição de um registro de filme em um arquivo, a partir de seu
+// ID (campo Movie::id). A função deve retornar a posição (índice) que o registro ocupa no arquivo ou
+// -1, caso ID inexistente. OBS: você não deve trazer todos os registros para a RAM, em um array. A
+// função deve acessar registro por registro, no próprio arquivo.
+int find_movie(const char* filepath, int id){
+    FILE* file = fopen(filepath, "r");
+    if (file == NULL){
+        printf("Deu ruim\n");
+        return -2;
+    }
+    // Movie* movies;
+    // fread(movies, sizeof(Movie), count_movies(filepath), file);
+    int size = count_movies(filepath);
+    for (int i = 0; i < size; i++){
+        Movie movie;
+        fread(&movie, sizeof(Movie), 1, file);
+        if (movie.id == id){
+            fclose(file);
+            return i;
+        }
+    }
+    fclose(file);
+    return -1;
+}
+
 int main(int argc, char const *argv[]){
     Movie list[] = {
         {6, "They Live"},
@@ -44,6 +92,10 @@ int main(int argc, char const *argv[]){
 
     char path[] = "movies";
     write_all_movies(path, list,7);
-    printf("%d", count_movies(path));
+    printf("%d\n", count_movies(path));
+    int pos = 2;
+    Movie movie = read_movie(path, pos);
+    printf("%d - Filme de ID <%d>: <%s>\n", pos, movie.id, movie.name);
+    printf("Posição do Filme com ID: <%d> = <%d>\n", 23, find_movie(path, 23));
     return 0;
 }
